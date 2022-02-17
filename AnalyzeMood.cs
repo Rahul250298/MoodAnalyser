58bf2eb63ce7f743bd5c2884fe968f5ce1b77d9b
rahul <rahulravi2502@gmail.com>
2022-02-17 17:55:20 +0530

    Add ability to analyze program

--- AnalyzeMood.cs
+++ AnalyzeMood.cs
@@ -8,46 +8,17 @@ namespace MoodAnalyser
 {
     public class AnalyzeMood
     {
-        public string message;
-        /// <summary>
-        /// default contructor
-        /// </summary>
-        public AnalyzeMood()
+        public string Mood(string message)
         {
-
-        }
-        //parameterized constructor
-        public AnalyzeMood(string message)
-        {
-            this.message = message;
-        }
-        //Method to return the type of Mood
-        public string Mood()
-        {
-            try
-            {
-                message = message.ToLower();
-                if (message == null)
-                {
-                    throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NULL_EXCEPTION, "Message cann't be null");
-                }
-                if (message.Equals(string.Empty))
-                {
-                    throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.EMPTY_EXCEPTION, "Message cann't be Empty");
-                }
-                if (message.Contains("happy"))
-                {
-                    return "happy";
-                }
-                else
-                {
-                    return "sad";
-                }
-            }
-            catch (NullReferenceException)
+            if (message.ToLower().Contains("happy"))
             {
                 return "happy";
+            }
+            else
+            {
+                return "sad";
             }
         }
     }
 }
+    
