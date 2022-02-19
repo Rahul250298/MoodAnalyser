80ad40c4fdcc95a46e5586687f50c5dddf349509
Rahul R <91786240+Rahul250298@users.noreply.github.com>
2022-02-19 12:12:51 +0530

    Ability to analyze mood

---
+++ MoodAnalyzerTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyser;
+using System;
+namespace MoodAnalyzerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("Happy Mood")]
+        public void GivenMessageShouldReturnHappy()
+        {
+            ///Follow AAA strategy
+            ///Arrange , Act and in last Assert
+            AnalyzeMood mood = new AnalyzeMood();
+            string message = "I am in Happy Mood";
+            string excepted = "happy";
+            var actual = mood.Mood(message);
+            Assert.AreEqual(excepted, actual);
+        }
+        [TestMethod]
+        [TestCategory("SAD Mood")]
+        public void GivenMessageShouldReturnSad()
+        {
+            ///Follow AAA strategy
+            ///Arrange , Act and in last Assert
+            AnalyzeMood mood = new AnalyzeMood();
+            string message = "I am in SAD Mood";
+            string excepted = "sad";
+            var actual = mood.Mood(message);
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
