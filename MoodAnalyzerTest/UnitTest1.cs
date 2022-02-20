d055ce4176d32a928769a9295fc740bf7b37b1f6
Rahul R <91786240+Rahul250298@users.noreply.github.com>
2022-02-20 17:35:37 +0530

    Check null and Empty exceptions

---
+++ MoodAnalyzerTest/UnitTest1.cs
@@ -0,0 +1,82 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyser;
+using System;
+namespace MoodAnalyzerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("Happy Mood")]
+        public void GivenMessageShouldReturnHappy()
+        {
+            ///Follow AAA strategy
+            ///Arrange , Act and in last Assert
+            AnalyzeMood mood = new AnalyzeMood();
+            string message = "I am in Happy Mood";
+            string excepted = "happy";
+            var actual = mood.Mood(message);
+            Assert.AreEqual(excepted, actual);
+        }
+        [TestMethod]
+        [TestCategory("SAD Mood")]
+        public void GivenMessageShouldReturnSad()
+        {
+            ///Follow AAA strategy
+            ///Arrange , Act and in last Assert
+            AnalyzeMood mood = new AnalyzeMood();
+            string message = "I am in SAD Mood";
+            string excepted = "sad";
+            var actual = mood.Mood(message);
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+    [TestMethod]
+    [TestCategory("Null Exception")]
+    public void GivenNullShouldReturnCustomException()
+    {
+        ///Follow AAA strategy
+        ///Arrange , Act and in last Assert
+        string message = null;
+        string excepted = "Message cann't be null";
+        try
+        {
+            AnalyzeMood mood = new AnalyzeMood(message);
+            string actual = mood.Mood();
+        }
+        catch (MoodAnalyzerException ex)
+        {
+            Console.WriteLine("Custom Exception :" + ex);
+            Assert.AreEqual(excepted, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Worst Case Exception :" + ex);
+        }
+    }
+    [TestMethod]
+    [TestCategory("Empty Exception")]
+    public void GivenEmptyShouldReturnCustomException()
+    {
+        ///Follow AAA strategy
+        ///Arrange , Act and in last Assert
+        string message = "";
+        string excepted = "Message cann't be Empty";
+        try
+        {
+            AnalyzeMood mood = new AnalyzeMood(message);
+            string actual = mood.Mood();
+        }
+        catch (MoodAnalyzerException ex)
+        {
+            Console.WriteLine("Custom Exception :" + ex);
+            Assert.AreEqual(excepted, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Worst Case Exception :" + ex);
+        }
+    }
+}
+
+   
